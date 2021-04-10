e8e54d7dcb336dc3443db4b6b57e829c4160c038
Ikboljon Zokirov <izokirov@outlook.com>
2021-04-10 19:11:25 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-const PORT = process.env.PORT || 85
+const PORT = process.env.PORT || 3000
 
 //using static files 
 app.use(express.static(path.join(__dirname, 'public')))
